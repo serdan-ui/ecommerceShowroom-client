981101af912c51afee533d6add63ace93f05f144
Sergio Rosales <sergio_dr@hotmail.com.ar>
2020-10-05 13:07:29 -0300

    changes 2

--- src/redux/actions/index.js
+++
@@ -1,8 +0,0 @@
-import { takeLatest } from 'redux-saga/effects';
-import { READ_REMINDERS } from './../ActionTypes';
-import { readRemindersSaga } from './reminderSaga';
-
-
-export function * watcherSaga(){
-    yield takeLatest(READ_REMINDERS ,  readRemindersSaga);
-}
